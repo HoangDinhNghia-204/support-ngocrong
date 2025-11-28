facefe5e5f640280cf659f03e53ed1c61c593734
Hoàng Nghĩa <nghiadz29112004@gmail.com>
2025-11-28 20:14:27 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // --- DỮ LIỆU KHỞI TẠO ---
 const rawData = [
   // Dữ liệu MỚI NHẤT (Ngày 27/11/2025)
+  { time: "20:05 - 28/11/2025", item: "Găng Thần Namếc", map: "Rừng tuyết" },
   { time: "18:05 - 28/11/2025", item: "Áo Thần Namếc", map: "Núi tuyết" },
   { time: "18:05 - 28/11/2025", item: "Găng Thần Namếc", map: "Rừng tuyết" },
   { time: "14:12 - 28/11/2025", item: "Áo Thần Namếc", map: "Cánh đồng tuyết" },
